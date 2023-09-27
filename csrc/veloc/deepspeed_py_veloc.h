4b60eb581a74e875b72aa7b5ff8ac9c7018e4cc3
amaurya <amaurya@anl.gov>
2023-09-27 21:16:19 +0000

    Moving to CPP ckpt

--- csrc/veloc/deepspeed_py_veloc.h
+++ csrc/veloc/deepspeed_py_veloc.h
@@ -1,13 +1,20 @@
 #include <stdlib.h>
+<<<<<<< HEAD
 
 #include <deque>
 #include <mutex>
 #include <torch/csrc/python_headers.h>
 #include <torch/csrc/utils/pybind.h>
+=======
+#include <torch/extension.h>
+#include <deque>
+#include <mutex>
+>>>>>>> 0df12df7 (Moving to CPP ckpt)
 #include <condition_variable>
 #include <cuda_runtime.h>
 #include <tuple>
 #include <iostream>
+<<<<<<< HEAD
 #include <exception>
 #include <ATen/core/Dict.h>
 #include <ATen/core/ivalue.h>
@@ -27,6 +34,9 @@
 #include <fstream>
 
 namespace py = pybind11;
+=======
+#include <ATen/core/Dict.h>
+>>>>>>> 0df12df7 (Moving to CPP ckpt)
 
 #define checkCuda(ans) { checkCudaFunc((ans), __FILE__, __LINE__); }
 inline void checkCudaFunc(cudaError_t code, const char *file, int line, bool abort=true) {
@@ -36,6 +46,7 @@ inline void checkCudaFunc(cudaError_t code, const char *file, int line, bool abo
    }
 }
 
+<<<<<<< HEAD
 class veloc_ckpt_t {
     char*_start_ptr;
     size_t _curr_offset = 0;
@@ -100,4 +111,34 @@ class veloc_ckpt_t {
     void _d2h_trf();
     void _h2f_trf();
     void shutdown();
+=======
+struct ckpt_t {
+    void *ptr;
+    size_t size;
+    size_t offset;
+};
+
+class veloc_ckpt_t {
+    typedef std::unordered_map<std::string, c10::IValue> ckpt_map_t;
+    void *_start_ptr;
+    size_t _curr_offset = 0;
+    size_t _total_host_cache = 0;
+    std::deque<ckpt_t> _pending;
+    std::mutex _mutex;
+    std::condition_variable _cv;
+    // std::unique_lock<std::mutex> _lock(_mutex, std::defer_lock);
+    std::thread copy_thread;
+
+    public:
+    veloc_ckpt_t(size_t host_cache) {
+        _total_host_cache = host_cache;
+        checkCuda(cudaMallocHost(&_start_ptr, host_cache));
+        _curr_offset = 0;
+        _pending.clear();
+    }
+
+    void ckpt(ckpt_map_t &m, std::string path);
+    void wait(const size_t tensor_id = 0);
+
+>>>>>>> 0df12df7 (Moving to CPP ckpt)
 };
